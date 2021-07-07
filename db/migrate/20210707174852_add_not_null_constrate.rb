28789d4c905cc119899a4a9bca08a62a3fb147c8
andrew <andrewtehanov@gmail.com>
2021-07-07 20:54:33 +0300

    Add not null constrate

---
+++ db/migrate/20210707174852_add_not_null_constrate.rb
@@ -0,0 +1,8 @@
+class AddNotNullConstrate < ActiveRecord::Migration[6.1]
+  def change
+    change_column_null :users, :first_name, false
+    change_column_null :users, :last_name, false
+    change_column_null :users, :email, false
+    change_column_null :users, :phone_number, false
+  end
+end
