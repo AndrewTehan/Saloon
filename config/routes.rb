9555ac7b8ac2ac8af0a6388faa5f175d4792a832
andrew <andrewtehanov@gmail.com>
2021-07-11 18:02:36 +0300

    Heroku first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  root 'users#index'
   resources :users, only: %i[index show destroy]
-  get '/me', to: 'users#me'
-  get 'me/edit', to: 'users#edit'
-  put 'me/edit/:id', to: 'users#update'
+  devise_for :users
+  # get '/me', to: 'users#me'
+  # get 'me/edit', to: 'users#edit'
+  # put 'me/edit/:id', to: 'users#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
