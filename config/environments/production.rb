65955bd95f4592d8cca397bce1f6ced8fcf325ff
andrew <andrewtehanov@gmail.com>
2021-07-20 00:24:41 +0300

    Setting up Gmail confirmations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "saloon_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {:Host => 'lit-wave-11568.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port    => 587,
+    :domain  => 'lit-wave-11568.herokuapp.com'
+    :authentication => :plain,
+    :enable_starttls_auto => true,
+    :user_name => 'andrewtehanov@gmail.com',
+    :password => 'androidGoogle'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
