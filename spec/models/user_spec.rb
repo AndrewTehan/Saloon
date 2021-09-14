67fe1530b847a7d74fe0040149243613daf6f6ee
andrew <andrewtehanov@gmail.com>
2021-09-15 00:02:00 +0300

    Tested user factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,25 +10,25 @@ RSpec.describe User, type: :model do
   end
 
   describe "user_factory" do
-    context "with invalid service" do
+    context "with invalid first_name" do
       let(:first_name){ "" }
       let(:user){ FactoryBot.create(:user, first_name: first_name) }
       include_examples "Raises validation error"
     end
 
-    context "with invalid service" do
+    context "with invalid last_name" do
       let(:last_name){ "" }
       let(:user){ FactoryBot.create(:user, last_name: last_name) }
       include_examples "Raises validation error"
     end
 
-    context "with invalid service" do
+    context "with invalid phone_number" do
       let(:phone_number){ "" }
       let(:user){ FactoryBot.create(:user, phone_number: phone_number) }
       include_examples "Raises validation error"
     end
 
-    context "with invalid service" do
+    context "with invalid email" do
       let(:email){ "" }
       let(:email){ FactoryBot.create(:user, email: email) }
       include_examples "Raises validation error"
