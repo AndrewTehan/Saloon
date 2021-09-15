64b1aecd303337ad2ea350606a95a0fd11e3048d
andrew <andrewtehanov@gmail.com>
2021-09-15 10:37:28 +0300

    Corrected seed. Add client factory trates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,4 @@
-# frozen_string_literal: true
-
-def create_user_attributes(first_name: nil, last_name: nil, email: nil, phone_number: nil, password: '111111')
-  {
-    first_name: first_name || Faker::Name.unique.first_name,
-    last_name: last_name || Faker::Name.unique.last_name,
-    email: email || Faker::Internet.unique.email,
-    phone_number: phone_number || Faker::PhoneNumber.cell_phone_in_e164,
-    password: password
-  }
-end
-
-admin = Admin.create(**create_user_attributes)
-client = Client.create(**create_user_attributes(first_name: 'Andrew', last_name: 'Tehanov', email: 'andrewtehanov@gmail.com', phone_number: '+375447756860'))
-
+FactoryBot.create(:user, type: Admin)
+FactoryBot.create(:client, :tehanov_last_name, :tehanov_first_name, :andrewtehanov_email)
+FactoryBot.create(:master)
 User.all.each(&:confirm)
-service_prices = [%w[Coloring 30],
-                  ['Men haircut', '15'],
-                  ['Women short haircut', '20'],
-                  ['Women long haircut', '25'],
-                  ['Gray Cammuffling', '10'],
-                  ["Children's Haircuts", '10'],
-                  ['Mustache and Beard Haircut', '13'],
-                  %w[Highlighting 28],
-                  %w[Lamination 45],
-                  %w[Toning 24],
-                  %w[Balayazh 27],
-                  ['Eyebrow Correction', '17'],
-                  ['Eyebrow & Eyelash Tint', '19']]
-
-services = service_prices.map do |(name, cost)|
-  Service.create(service_name: name, cost: cost)
-end
-
-services.each_slice(6) do |master_services|
-  master_services_attributes = master_services.map do |service|
-    { service_id: service.id }
-  end
-
-  master = Master.create(
-    **create_user_attributes,
-    master_services_attributes: master_services_attributes
-  )
-end
--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -3,5 +3,17 @@
 FactoryBot.define do
   factory :client, parent: :user do
     initialize_with { Client.new }
-  end
+
+    trait :andrewtehanov_email do
+      email { "andrewtehanov@gmail.com" }
+    end
+
+    trait :tehanov_last_name do
+      last_name { "tehanov" }
+    end
+
+    trait :tehanov_first_name do
+      first_name { "andrew" }
+    end
+  end  
 end
