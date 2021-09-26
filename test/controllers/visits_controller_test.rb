84f6c8d7f8e56985e95734f18474bdc69ab150d7
andrew <andrewtehanov@gmail.com>
2021-09-26 18:47:14 +0300

    Deleted minitest files(test folder)

--- test/controllers/visits_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class VisitsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/master_service_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class MasterServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
