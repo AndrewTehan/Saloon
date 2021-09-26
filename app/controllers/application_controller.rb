0aefdab50ea7f5a65ead19ebd08144a581e88c95
andrew <andrewtehanov@gmail.com>
2021-09-26 19:32:38 +0300

    Changed find_by to find in find_user. Set private find_user. Render 404 unless find_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActionController::RoutingError, with: -> { render_404 } unless Rails.application.config.consider_all_requests_local
-
+  rescue_from ActiveRecord::RecordNotFound, with: -> { render_404 } unless Rails.application.config.consider_all_requests_local
+  
   protected
-
   def default_url_options
     { locale: I18n.locale }
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def find_user
-    User.find_by(id: params[:id])
-  end
-
   def index
     @user = current_user
     @users = User.all
@@ -50,5 +46,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(@user.is_a?(Admin) ? :admin : :client).permit(:first_name, :last_name, :email, :phone_number)
+  end
+
+  def find_user
+    User.find(params[:id]) 
   end
 end
