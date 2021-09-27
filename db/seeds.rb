3dfa7be2bcda44597af668e1bc5067e81552f926
andrew <andrewtehanov@gmail.com>
2021-09-27 17:46:11 +0300

    Corrected seed. Added services factory traits. Changed master factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,21 @@
 FactoryBot.create(:user, type: Admin)
+
 FactoryBot.create(:client, :tehanov_last_name, :tehanov_first_name, :andrewtehanov_email)
-FactoryBot.create(:master)
-User.all.each(&:confirm)
+
+FactoryBot.create(:service, :coloring)
+FactoryBot.create(:service, :men_haircut)
+FactoryBot.create(:service, :women_short_haircut)
+FactoryBot.create(:service, :women_long_haircut)
+FactoryBot.create(:service, :gray_cammuffling)
+FactoryBot.create(:service, :childre_haircuts)
+FactoryBot.create(:service, :mustache_and_beard_haircut)
+FactoryBot.create(:service, :highlighting)
+FactoryBot.create(:service, :lamination)
+FactoryBot.create(:service, :toning)
+FactoryBot.create(:service, :balayazh)
+FactoryBot.create(:service, :eyebrow_correction)
+FactoryBot.create(:service, :women_haircut)
+
+FactoryBot.create(:master, :coloring)
+FactoryBot.create(:master, :gray_cammuffling)
+FactoryBot.create(:master, :lamination)
